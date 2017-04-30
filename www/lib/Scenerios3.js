8ce18a9ac4d920db30a5d246fcad3658985fcaa8
emily trans <emily trans>
2017-04-30 14:35:03 +1000

    hukn

---
+++ www/lib/Scenerios3.js
@@ -0,0 +1,356 @@
+[
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 14.4,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 17.28,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 20.736,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 24.8832,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 29.85984,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 35.831808,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 42.9981696,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 51.59780352,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 61.91736422,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 74.30083707,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 89.16100448,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 106.9932054,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 128.3918465,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 154.0702157,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 184.8842589,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 221.8611107,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 266.2333328,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 319.4799994,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 383.3759992,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 460.0511991,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 552.0614389,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 662.4737267,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 794.968472,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 953.9621664,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 1144.7546,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 1373.70552,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 1648.446624,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 1978.135948,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 2373.763138,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 2848.515766,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 46.27526276
+  },
+  {
+    "balance": 3418.218919,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 55.73031531
+  },
+  {
+    "balance": 4101.862702,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 67.07637837
+  },
+  {
+    "balance": 4922.235243,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 80.69165405
+  },
+  {
+    "balance": 5906.682292,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 97.02998486
+  },
+  {
+    "balance": 7088.01875,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 116.6359818
+  },
+  {
+    "balance": 8505.6225,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 140.1631782
+  },
+  {
+    "balance": 10206.747,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 168.3958138
+  },
+  {
+    "balance": 12248.0964,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 202.2749766
+  },
+  {
+    "balance": 14697.71568,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 242.9299719
+  },
+  {
+    "balance": 17637.25882,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 291.7159663
+  },
+  {
+    "balance": 21164.71058,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 350.2591596
+  },
+  {
+    "balance": 25397.65269,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 420.5109915
+  },
+  {
+    "balance": 30477.18323,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 504.8131898
+  },
+  {
+    "balance": 36572.61988,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 605.9758277
+  },
+  {
+    "balance": 43887.14386,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 727.3709933
+  },
+  {
+    "balance": 52664.57263,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 873.0451919
+  },
+  {
+    "balance": 63197.48715,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 1047.85423
+  },
+  {
+    "balance": 75836.98458,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 1257.625076
+  },
+  {
+    "balance": 91004.3815,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 1509.350092
+  },
+  {
+    "balance": 109205.2578,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 1811.42011
+  },
+  {
+    "balance": 131046.3094,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 2173.904132
+  },
+  {
+    "balance": 157255.5712,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 2608.884958
+  },
+  {
+    "balance": 188706.6855,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 3130.86195
+  },
+  {
+    "balance": 226448.0226,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 3757.23434
+  },
+  {
+    "balance": 271737.6271,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 4508.881208
+  },
+  {
+    "balance": 326085.1525,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 5410.85745
+  },
+  {
+    "balance": 391302.183,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 6493.22894
+  },
+  {
+    "balance": 469562.6196,
+    "account ID": 415098,
+    "$afe_Index": 0.2,
+    "$ave_Index": 7792.074728
+  }
+]
---
+++ www/lib/scenerios5.js
@@ -0,0 +1,356 @@
+[
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": "",
+    "$ave_Index": ""
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  },
+  {
+    "balance": 12,
+    "account ID": 415098,
+    "$afe_Index": 0,
+    "$ave_Index": 0
+  }
+]
